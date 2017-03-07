0893f1f0be47e59a4f34d67da03714fee7e0140f
Justyna <justynazarudzka@gmail.com>
2017-03-07 10:40:04 +0100

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,17 @@
+var main = setInterval(function() {
+  if ($('header').hasClass('white')) {
+    $('header').removeClass('white');
+    $('header').addClass('pink');
+  } else if ($('header').hasClass('pink')) {
+    $('header').removeClass('pink');
+    $('header').addClass('magenta');
+  } else if ($('header').hasClass('magenta')){
+    $('header').removeClass('magenta');
+    $('header').addClass('pink2');
+  } else {
+    $('header').removeClass('pink2');
+    $('header').addClass('white')
+  }
+    
+}, 3000);
+$(document).ready(main);
